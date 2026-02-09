e884676922829016315294715d2c51acceec6b6d
Indra Sah Noeldy <twinightwheel50@gmail.com>
2026-02-09 09:24:30 +0700

    Add item definitions for telegram and WhatsApp bots

---
+++ tools/items.js
@@ -0,0 +1,19 @@
+// Item definition for the items system in the telegram bot 
+const items_serpulo = {
+    // Item: price,
+    Copper: 50,
+    Lead: 100,
+    Titanium: 250,
+    Thorium: 500,
+    Plastanium: 1000
+};
+
+// Item definition for the items system in the WhatsApp bot 
+const items_erekir = {
+    Beryllium: 100,
+    Graphite: 200,
+    Tungsten: 500,
+    Thorium: 1000,
+    Oxide: 2000,
+    Carbide: 5000
+};
