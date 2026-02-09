ea52e07e715ba40348aeb74fee9741391e3e4800
Indra Sah Noeldy <twinightwheel50@gmail.com>
2026-02-09 09:27:10 +0700

    Zzz

--- tools/items.js
+++ tools/items.js
@@ -1,11 +1,10 @@
 // Item definition for the items system in the telegram bot 
 const items_serpulo = {
     // Item: price,
-    Copper: 50,
+    Copper: 75,
     Lead: 100,
     Titanium: 250,
     Thorium: 500,
-    Plastanium: 1000
 };
 
 // Item definition for the items system in the WhatsApp bot 
